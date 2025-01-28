a3019e66145720e57c488502aee58baead29d73f
SashkaBeardman <lovkin95@gmail.com>
2025-01-28 16:25:27 +0100

    Dz Day4.2

---
+++ Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+double a, b;
+char CalcAction;
+Console.Write("Введите a: ");
+    a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b: ");
+    b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите действие (+, -, *, /): ");
+    CalcAction = Convert.ToChar(Console.ReadLine());
+
+    Console.WriteLine("\n==== Решение примера ====");
+    switch (CalcAction)
+    {
+        case '+':
+            Console.WriteLine($"{a}+{b} = {a + b}");
+            break;
+        case '-':
+            Console.WriteLine($"{a}-{b} = {a -+ b}");
+        break;
+        case '*':
+            Console.WriteLine($"{a}*{b} = {a * b}");
+        break;
+        case '/':
+            Console.WriteLine($"{a}/{b} = {a / b}");
+        break;
+        default:
+            Console.WriteLine("Ошибка, введен некорректный знак математической операции");
+            break;
+    }
