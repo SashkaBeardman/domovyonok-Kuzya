e23a364bca0d1e5257238cee7a41120ac54b5240
SashkaBeardman <lovkin95@gmail.com>
2025-01-28 15:46:34 +0100

    Dz Day4.1

---
+++ NameDay/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.Write("Введите номер дня недели (1-7):");
+    int dayNumber = int.Parse(Console.ReadLine());
+
+static string GetDayName(int dayNumber)
+{
+    switch (dayNumber)
+    {
+        case 1: return "Понедельник";
+        case 2: return "Вторник";
+        case 3: return "Среда";
+        case 4: return "Четверг";
+        case 5: return "Пятница";
+        case 6: return "Субота";
+        case 7: return "Воскресенье";
+        default: return null;
+    }
+}
+
+string dayName = GetDayName(dayNumber);
+
+        if (dayName != null)
+        {
+            Console.WriteLine($"День недели: {dayName}");
+        }
+        else
+        {
+            Console.WriteLine("Номер введен не верно. Введите число от 1 до 7.");
+        }
+    
+
+
