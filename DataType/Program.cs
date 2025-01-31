2da2ce1092bb392b6d28c0cb19bad9d9b4fe61f5
SashkaBeardman <lovkin95@gmail.com>
2025-01-31 16:25:06 +0100

    Dz Day5.2

---
+++ DataType/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число:");
+try
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"Вы ввели число: {a}");
+}
+catch
+{
+    Console.Write("Значение не является числом");
+}
