587224637f068087a640ba83a290348b4530743b
SashkaBeardman <lovkin95@gmail.com>
2025-02-10 07:09:25 +0100

    Dz_6,1 UP

--- DZ_6,1/Проверка пароля.cs
+++ DZ_6,1/Проверка пароля.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.Design;
+
 Console.WriteLine("\n==== Проверка пароля ====\n");
 Console.Write("Введите пароль: ");
 string pass = Console.ReadLine();
@@ -5,15 +7,25 @@ bool VerifiPass;
 if (pass.Length >= 8)
 {
     VerifiPass = pass.Any(char.IsLetter);
+    int passLetter = VerifiPass ? 1 : 0;
     Console.Write(VerifiPass ? "" : "Пароль не содержит букв / ");
     VerifiPass = pass.Any(char.IsDigit);
+    int passDigit = VerifiPass ? 1 : 0;
     Console.Write(VerifiPass ? "" : "Пароль не содержит цифр / ");
     VerifiPass = pass.Any(char.IsPunctuation);
+    int passPunctuation = VerifiPass ? 1 : 0;
     Console.Write(VerifiPass ? "" : "Пароль не содержит знаков препинания / ");
     VerifiPass = pass.Any(char.IsSymbol);
+    int passSymbol = VerifiPass ? 1 : 0;
     Console.WriteLine(VerifiPass ? "" : "Пароль не содержит символов");
-    bool result = pass.Any(c => char.IsLetter(c)) && pass.Any(c => char.IsDigit(c)) && pass.Any(c => char.IsPunctuation(c)) && pass.Any(c => char.IsSymbol(c));
-    Console.WriteLine(result ? "-----------------\n| Пароль принят |\n-----------------" : "--------------------\n| Пароль не принят |\n--------------------");
+    if ((passLetter + passDigit + passPunctuation + passSymbol) == 4)
+    {
+        Console.WriteLine("-----------------\n| Пароль принят |\n-----------------");
+    }
+    else
+    {
+        Console.WriteLine("--------------------\n| Пароль не принят |\n--------------------");
+    }
 }
 else
 {
