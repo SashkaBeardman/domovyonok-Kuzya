8629c25c8a996a8909a67f9304dc54752e850935
SashkaBeardman <lovkin95@gmail.com>
2025-03-16 11:38:57 +0100

    upd VegShop

--- DZ_6,1/Таблица умножения.cs
+++
@@ -1,8 +0,0 @@
-Console.WriteLine("\n\n==== Таблица умножения ====\n");
-Console.Write("Введите число для вывода таблици умножения: ");
-
-int multiplication = int.Parse(Console.ReadLine());
-for (int i = 1; i < 11; i++)
-{
-    Console.WriteLine($"{multiplication}*{i}=" + i * multiplication);
-}
